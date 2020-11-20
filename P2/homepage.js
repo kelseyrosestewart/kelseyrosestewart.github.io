e48d100afb4d597378d439d827cc5950c58d5f0a
kelseyrosestewart <72037977+kelseyrosestewart@users.noreply.github.com>
2020-11-19 17:58:44 -0800

    P2 Work

---
+++ P2/homepage.js
@@ -0,0 +1,47 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+'use strict'
+
+
+function toggleIcon() {
+    $('.icon').on('click', function(){
+        $('.icon').toggleClass('active');
+        $('.menu-mobile').slideToggle(300);
+    })
+}
+
+function closeMobileMenu(){
+    $('.menu-mobile').on('click', 'a', function(){
+        $('.icon').trigger('click');
+    })
+}
+
+
+
+function desktopMenu() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50 && $(window).width() > 768) {
+            $('header.desktop').fadeIn(500);
+        } else {
+            $('header.desktop').fadeOut(500);
+        }
+    })
+}
+
+//when the page loads call toggleIcon;
+$(toggleIcon);
+$(closeMobileMenu);
+$(desktopMenu);
