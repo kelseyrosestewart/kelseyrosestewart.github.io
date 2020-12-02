f10dab8ec6c14813c96f9254a8fb719d3adb5be0
kelseyrosestewart <72037977+kelseyrosestewart@users.noreply.github.com>
2020-12-02 10:59:26 -0800

    Demo Updates

--- P2/modal.js
+++ P2/modal.js
@@ -32,10 +32,10 @@
   
     return {
       center: function(){
-        var top = Math.max($window.height() - $modal.outerHeight(), 0) / 2;
+        var top = 0;
         var left = Math.max($window.width() - $modal.outerWidth(), 0) / 2;
         $modal.css({
-          top: top + $window.scrollTop(),
+          top: top ,
           left: left + $window.scrollLeft(),
         });
       },
